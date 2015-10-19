5fc54d53e9ebfeacd461858b80fe86150a24fa92
Conner Fullerton <connerfullerton@conners-mac-mini.local>
2015-10-19 16:32:31 -0700

    Initial Commit

---
+++ Tack!/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tack!
+//
+//  Created by Conner Fullerton on 10/19/15.
+//  Copyright © 2015 Conner Fullerton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Tack!Tests/Tack_Tests.m
@@ -0,0 +1,39 @@
+//
+//  Tack_Tests.m
+//  Tack!Tests
+//
+//  Created by Conner Fullerton on 10/19/15.
+//  Copyright © 2015 Conner Fullerton. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Tack_Tests : XCTestCase
+
+@end
+
+@implementation Tack_Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
